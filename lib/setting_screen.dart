71426cd6fd1dcd1738fd88f4ff9ce821bc31ff11
Phạm Thanh Tường <tuongpt12503@gmail.com>
2024-05-04 23:58:04 +0700

    save data and fix bug done

--- lib/setting_screen.dart
+++ lib/setting_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -8,8 +11,41 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  String imagefile = '';
+  @override
+  void initState() {
+    super.initState();
+    _loadImage();
+  }
+
+  _loadImage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      imagefile = prefs.getString('avatar') ?? '';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: Colors.white,
+      child: Container(
+        width: 160.0,
+        height: 160.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: imagefile.isNotEmpty
+              ? DecorationImage(
+                  image: NetworkImage(imagefile),
+                  fit: BoxFit.cover,
+                )
+              : DecorationImage(
+                  image:
+                      AssetImage("assets/images/default_profile_picture.png"),
+                  fit: BoxFit.cover,
+                ),
+        ),
+      ),
+    );
   }
 }
