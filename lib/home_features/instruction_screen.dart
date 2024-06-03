53aa9b22d8973e47e4e0599c514496baff1bd1aa
Phạm Thanh Tường <tuongpt12503@gmail.com>
2024-06-03 23:51:06 +0700

    add instruction for game

--- lib/home_features/instruction_screen.dart
+++ lib/home_features/instruction_screen.dart
@@ -64,37 +64,49 @@ class _InstructionScreenState extends State<InstructionScreen> {
                       Padding(
                         padding: EdgeInsets.symmetric(horizontal: 20),
                         child: Text(
-                          GameInstructions.instructions,
+                          GameInstructions.instructions +
+                              "\n2. Tính quan trọng của từng lá bài trong Xì Dách\n" +
+                              "- Các lá bài từ 2 đến 10 có giá trị điểm tương ứng với số trên lá. Ví dụ, lá bài 3 (cơ) sẽ có giá trị là 3 điểm.\n" +
+                              "- Các lá bài J, Q và K đều được tính là 10 điểm.\n" +
+                              "3. Hướng dẫn chơi Xì Dách cơ bản\n" +
+                              "Một người sẽ là người chia bài, sử dụng bộ bài 52 lá để phân phối cho mỗi người tham gia (nhà con) và cho mình (nhà cái) là 2 lá.\n" +
+                              "Sau đó, người chơi có thể rút thêm tối đa 3 lá bài nữa, tức là người chơi có thể có tới 5 lá bài trong mỗi ván.\n" +
+                              "Mục tiêu của việc rút bài này là để tổng giá trị các lá bài mình cầm gần hoặc bằng 21 càng tốt. Nếu không ai vượt qua được điểm số của bạn mà không quá 21 điểm, bạn sẽ tăng cơ hội chiến thắng của mình lên cao.\n" +
+                              "Ở giai đoạn 1: Tính điểm của 2 lá bài\n" +
+                              "- Khi bạn có bộ bài như vậy, bạn sẽ được hưởng lợi:\n" +
+                              "+ Xì bàng: 2 lá Át\n" +
+                              "+ Xì dách: 1 lá Át và 1 lá 10, J, Q hoặc K\n" +
+                              "Nếu nhà cái có bài như vậy, cơ hội chiến thắng của nhà con sẽ cao hơn (trừ khi nhà con có bài bằng hoặc cao hơn)\n" +
+                              "Ở giai đoạn 2: Rút thêm lá\n" +
+                              "- Điều 1: Mỗi người được nhận 2 lá bài\n" +
+                              "- Điều 2: Mỗi người kiểm tra bài, tính điểm và quyết định có rút thêm lá không.\n" +
+                              "- Điều 3: Khi mọi người ngừng rút, đến lượt nhà cái. Nhà cái có quyền xét duyệt và tiếp tục rút bài.\n" +
+                              "Sau khi tính điểm 2 lá bài ban đầu, người chơi có thể rút thêm hoặc dừng để tổng điểm từ 16 - 21.\n" +
+                              "+ Không đủ: Bài trên tay dưới 16 điểm\n" +
+                              "+ Đủ: Bài trên tay có điểm từ 16 - 21\n" +
+                              "+ Vượt quá (quắc): Bài có điểm cao hơn 21. Người nào có điểm lớn hơn 21 sẽ không được rút thêm\n" +
+                              "Vòng chơi: Bắt đầu từ nhà cái theo chiều kim đồng hồ. Mỗi người sẽ rút bài cho đến khi dừng. Người chơi cần có trên 16 điểm trước khi so bài. Nếu không, nhà con sẽ thua. Sau khi so bài, nếu nhà cái cao hơn nhà con, nhà cái sẽ chiến thắng. Ngược lại, nhà con sẽ thắng. Nếu hai bên bằng điểm nhau, sẽ hòa.\n" +
+                              "4. Tình huống đặc biệt khi chơi Xì dách\n" +
+                              "Người chơi sẽ đối mặt với các tình huống sau:\n" +
+                              "- Ngũ linh: Tổng điểm 5 lá bài nhỏ hơn 21 sẽ dẫn đến chiến thắng tuyệt đối. Trong trường hợp cả hai đều có ngũ linh, người có tổng điểm thấp hơn sẽ thắng.\n" +
+                              "- Vượt quá 21 điểm: Nếu cả hai vượt quá, người có tổng điểm thấp hơn sẽ chiến thắng. Trong trường hợp vượt quá 21 điểm, người có tổng điểm dưới 16 điểm sẽ thua.\n" +
+                              "5. Lưu ý\n" +
+                              "Khi tham gia Xì dách, bạn sẽ gặp các tình huống sau và áp dụng cách tính điểm như sau:\n" +
+                              "- Cả nhà cái và người chơi có bài quắc (vượt quá 21) hoặc cùng điểm, kết quả là hòa, không ai thua cược.\n" +
+                              "- Cả nhà cái và người chơi có Xì bàng, lúc này nhà cái sẽ giành chiến thắng.\n" +
+                              "- Cả nhà cái và người chơi có Xì dách, nhà cái sẽ là người thắng cuộc.\n" +
+                              "- Cả nhà cái và người chơi có ngũ linh, người có tổng điểm ít hơn sẽ thắng.\n" +
+                              "6. Thuật ngữ Xì dách quan trọng\n" +
+                              "Khi tham gia Xì dách trực tuyến, bạn thường xuyên gặp các thuật ngữ sau. Hãy tham khảo để trở thành một cao thủ Xì dách.\n" +
+                              "- Rút thêm: Bổ sung 1 lá vào bộ bài\n" +
+                              "- Dừng: Không cần thêm lá nào nữa\n" +
+                              "- Gấp đôi: Đặt cược gấp đôi và nhận thêm 1 lá\n" +
+                              "- Tách bài: Khi hai lá đầu giống nhau, bạn có thể tách thành hai tay và chơi tiếp. Mỗi tay có thêm 1 lá, cược như ban đầu.\n" +
+                              "- Bảo hiểm: Khi nhà cái có lá Át, bạn có thể chọn bảo hiểm. Quyết định này thuộc về bạn, có đặt cược hay không.\n",
                           style: TextStyles.instructions,
                         ),
                       ),
                       const Gap(20),
-                      Row(children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          child: RichText(
-                            text: TextSpan(
-                              style: TextStyles.instructions,
-                              children: [
-                                TextSpan(
-                                  text: 'Tham khảo thêm tại: ',
-                                ),
-                                TextSpan(
-                                  text: '\nHướng dẫn cơ bản BlackJack',
-                                  style: TextStyles.instructions.copyWith(
-                                      color: Palette.numberText,
-                                      decoration: TextDecoration.underline),
-                                  recognizer: TapGestureRecognizer()
-                                    ..onTap = () {
-                                      launchUrl(
-                                          GameInstructions.instructionsLink);
-                                    },
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ]),
                       const Gap(30),
                       CustomElevatedButtonSmall(
                         width: 150,
