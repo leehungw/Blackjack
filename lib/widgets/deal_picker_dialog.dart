3c5e81e6fd31bca651468ce180f9149256449925
Phạm Thanh Tường <tuongpt12503@gmail.com>
2024-06-07 22:18:56 +0700

    update UI

--- lib/widgets/deal_picker_dialog.dart
+++ lib/widgets/deal_picker_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:card/GameObject/game_online_manager.dart';
 import 'package:card/style/palette.dart';
+import 'package:card/style/text_styles.dart';
 import 'package:card/widgets/custom_elevated_button_big.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -32,121 +33,135 @@ class _DealPickerDialogState extends State<DealPickerDialog> {
     return AlertDialog(
       content: Wrap(children: [
         Container(
-          constraints: const BoxConstraints(),
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8),
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(12),
               gradient: LinearGradient(colors: const [
                 Palette.homeDialogBackgroundGradientBottom,
-                Palette.homeDialogBackgroundGradientTop
+                Colors.blueAccent,
               ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Row(),
-              const Gap(30),
-              if (widget.thisPlayer.money > 10000) CustomElevatedButtonBig(
-                width: 150,
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: const [
-                    Palette.homeDialogPrimaryButtonBackgroundGradientBottom,
-                    Palette.homeDialogPrimaryButtonBackgroundGradientTop
-                  ],
+              Container(
+                alignment: Alignment.center,
+                child: Text(
+                  "Chọn mức cược",
+                  style: TextStyles.screenTitle.copyWith(
+                    color: Palette.black,
+                    fontSize: 26,
+                  ),
                 ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  gameManager.reqReady(10000);
-                },
-                text: "10.000Đ",
               ),
-              if (widget.thisPlayer.money > 10000) const Gap(30),
-              if (widget.thisPlayer.money > 20000) CustomElevatedButtonBig(
-                width: 150,
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: const [
-                    Palette.homeDialogPrimaryButtonBackgroundGradientBottom,
-                    Palette.homeDialogPrimaryButtonBackgroundGradientTop
-                  ],
+              const Gap(5),
+              if (widget.thisPlayer.money > 10000)
+                CustomElevatedButtonBig(
+                  width: 130,
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: const [
+                      Color.fromARGB(255, 252, 228, 75),
+                      Colors.deepOrange,
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    gameManager.reqReady(10000);
+                  },
+                  text: "10.000 Đ",
                 ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  gameManager.reqReady(20000);
-                },
-                text: "20.000Đ",
-              ),
-              if (widget.thisPlayer.money > 20000) const Gap(30),
-              if (widget.thisPlayer.money > 50000) CustomElevatedButtonBig(
-                width: 150,
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: const [
-                    Palette.homeDialogPrimaryButtonBackgroundGradientBottom,
-                    Palette.homeDialogPrimaryButtonBackgroundGradientTop
-                  ],
+              if (widget.thisPlayer.money > 10000) const Gap(15),
+              if (widget.thisPlayer.money > 20000)
+                CustomElevatedButtonBig(
+                  width: 130,
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: const [
+                      Color.fromARGB(255, 252, 228, 75),
+                      Colors.deepOrange,
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    gameManager.reqReady(20000);
+                  },
+                  text: "20.000 Đ",
                 ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  gameManager.reqReady(50000);
-                },
-                text: "50.000Đ",
-              ),
-              if (widget.thisPlayer.money > 50000) const Gap(30),
-              if (widget.thisPlayer.money > 100000) CustomElevatedButtonBig(
-                width: 150,
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: const [
-                    Palette.homeDialogPrimaryButtonBackgroundGradientBottom,
-                    Palette.homeDialogPrimaryButtonBackgroundGradientTop
-                  ],
+              if (widget.thisPlayer.money > 20000) const Gap(15),
+              if (widget.thisPlayer.money > 50000)
+                CustomElevatedButtonBig(
+                  width: 130,
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: const [
+                      Color.fromARGB(255, 252, 228, 75),
+                      Colors.deepOrange,
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    gameManager.reqReady(50000);
+                  },
+                  text: "50.000 Đ",
                 ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  gameManager.reqReady(100000);
-                },
-                text: "100.000Đ",
-              ),
-              if (widget.thisPlayer.money > 100000) const Gap(30),
-              if (widget.thisPlayer.money > 200000) CustomElevatedButtonBig(
-                width: 150,
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: const [
-                    Palette.homeDialogPrimaryButtonBackgroundGradientBottom,
-                    Palette.homeDialogPrimaryButtonBackgroundGradientTop
-                  ],
+              if (widget.thisPlayer.money > 50000) const Gap(15),
+              if (widget.thisPlayer.money > 100000)
+                CustomElevatedButtonBig(
+                  width: 130,
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: const [
+                      Color.fromARGB(255, 252, 228, 75),
+                      Colors.deepOrange,
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    gameManager.reqReady(100000);
+                  },
+                  text: "100.000 Đ",
                 ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  gameManager.reqReady(200000);
-                },
-                text: "200.000Đ",
-              ),
-              if (widget.thisPlayer.money > 200000) const Gap(30),
-              if (widget.thisPlayer.money > 500000) CustomElevatedButtonBig(
-                width: 150,
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: const [
-                    Palette.homeDialogPrimaryButtonBackgroundGradientBottom,
-                    Palette.homeDialogPrimaryButtonBackgroundGradientTop
-                  ],
+              if (widget.thisPlayer.money > 100000) const Gap(15),
+              if (widget.thisPlayer.money > 200000)
+                CustomElevatedButtonBig(
+                  width: 130,
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: const [
+                      Color.fromARGB(255, 252, 228, 75),
+                      Colors.deepOrange,
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    gameManager.reqReady(200000);
+                  },
+                  text: "200.000 Đ",
                 ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  gameManager.reqReady(500000);
-                },
-                text: "500.000Đ",
-              ),
-              if (widget.thisPlayer.money > 500000) const Gap(30),
+              if (widget.thisPlayer.money > 200000) const Gap(15),
+              if (widget.thisPlayer.money > 500000)
+                CustomElevatedButtonBig(
+                  width: 130,
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: const [
+                      Color.fromARGB(255, 252, 228, 75),
+                      Colors.deepOrange,
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    gameManager.reqReady(500000);
+                  },
+                  text: "500.000 Đ",
+                ),
+              if (widget.thisPlayer.money > 500000) const Gap(15),
             ],
           ),
         ),
